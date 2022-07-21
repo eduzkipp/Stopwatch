759413fa9510e728d91de176d6d98eefa891d07a
Edwin kiplangat <eduzkipp@gmail.com>
2022-07-21 21:43:49 +0300

    First commmit

---
+++ main.js
@@ -0,0 +1,50 @@
+const el_time=document.querySelector('.watch .time');
+const start_btn=document.getElementById('start');
+const stop_btn=document.getElementById('stop');
+const reset_btn=document.getElementById('reset');
+
+
+let seconds=00;
+let interval =null;
+
+start_btn.addEventListener('click', start);
+stop_btn.addEventListener('click', stop);
+reset_btn.addEventListener('click', reset);
+
+
+
+
+function timer(){
+seconds++;
+     let hrs=Math.floor(seconds/3600);
+     let mins=Math.floor((seconds-(hrs*3600))/60);
+    let secs =seconds%60;
+    
+      if (secs<10) {secs ='0' +secs;}
+    if (mins<10){mins ='0' +mins;}
+    if (hrs<10){hrs ='0' +hrs;}
+   
+    el_time.innerText =`${hrs}:${mins}:${secs}`;
+  
+   
+    
+}
+timer();
+
+function start (){
+    if(interval){
+        return
+    }
+    
+    interval=setInterval(timer, 1000);
+    
+}
+
+function stop(){
+    clearInterval(interval);
+    interval=null;
+}
+function reset(){
+    stop();
+    el_time.innerText='00:00:00';
+}
